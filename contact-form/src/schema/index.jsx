fee94ae287ebbc5d31b82d098f878d99e32009ba
Zohaib Azam <66849463+Zohaib58@users.noreply.github.com>
2023-07-27 13:47:11 +0500

    Amendment 1; Changed 20 char lim to 2k

--- contact-form/src/schema/index.jsx
+++ contact-form/src/schema/index.jsx
@@ -1,9 +1,9 @@
 import * as Yup from "yup"
 
 export const contactFormSchema = Yup.object({
-    name: Yup.string().min(2, "Atleast two characters").max(20, "Less than 20 characters please").required('Name is required'),
+    name: Yup.string().min(2, "Atleast two characters").max(2000, "Less than 2000 characters please").required('Name is required'),
     email: Yup.string().email().required('Email is required'),
-    address: Yup.string().min(10, "Atleast 10 characters required").max(20, "Less than 20 characters please").required('Address is required'),
-    phone: Yup.string().min(10, "Atleast 10 charcters required").max(20, "Less than 20 characters please").required('Phone is required')
+    address: Yup.string().min(10, "Atleast 10 characters required").max(2000, "Less than 2000 characters please").required('Address is required'),
+    phone: Yup.string().min(10, "Atleast 10 charcters required").max(2000, "Less than 2000 characters please").required('Phone is required')
 
 })
